8b8703fab367e8688d638e1dc472505f4f4d496e
eptrubnikova <eptrubnikova@gmail.com>
2022-10-14 12:47:32 +0600

    task 2

--- src/Main.java
+++ src/Main.java
@@ -9,16 +9,18 @@ public class Main {
             arr[0] = 1;
             arr[1] = 2;
             arr[2] = 3;
-            System.out.println(arr[1]);
+            System.out.print(arr[0] + ", " + arr[1] + ", " + arr[2]);
+            System.out.println();
         }
 
         {
             double[] arr = {1.57, 7.654, 9.986};
-            System.out.println(arr[0]);
+            System.out.println(arr[0] + ", " + arr[1] + ", " + arr[2]);
+
         }
         {
             int[] arr = {15, 18, 30, 78, 5};
-            System.out.println(arr[4]);
+            System.out.println(arr[0] + ", " + arr[1] + ", " + arr[2] + ", " + arr[3] + ", " + arr[4]);
         }
     }
 }
