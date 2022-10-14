ea43280e8336c2452b892abf0a33f7d9d00ed476
eptrubnikova <eptrubnikova@gmail.com>
2022-10-14 13:19:23 +0600

    task 3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,20 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
+
+    private static void task3() {
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        for (int x = 0; x < arr.length; x++)
+        if (arr[x] % 2 != 0)
+        System.out.print((arr[x]+1) + " ");
+        else System.out.print(arr[x] + " ");
+        System.out.println();
+            }
 
     private static void task2() {
         {
