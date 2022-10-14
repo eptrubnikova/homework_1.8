db606e21e2d3012f399ebcd5f132626bb6e4b08e
eptrubnikova <eptrubnikova@gmail.com>
2022-10-14 12:43:08 +0600

    task 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,23 @@ public class Main {
     public static void main(String[] args) {
         task1();
     }
-    public static void task1() {
 
+    public static void task1() {
+        {
+            int[] arr = new int[3];
+            arr[0] = 1;
+            arr[1] = 2;
+            arr[2] = 3;
+            System.out.println(arr[1]);
+        }
+
+        {
+            double[] arr = {1.57, 7.654, 9.986};
+            System.out.println(arr[0]);
+        }
+        {
+            int[] arr = {15, 18, 30, 78, 5};
+            System.out.println(arr[4]);
+        }
     }
 }
