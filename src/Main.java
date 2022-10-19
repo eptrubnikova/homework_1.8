f2d2e0bdde45b85a1efdfa9631e9d3e6eed4c445
eptrubnikova <eptrubnikova@gmail.com>
2022-10-19 14:07:54 +0600

    adjustment made 2

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,9 @@ public class Main {
             if (arr[x] % 2 != 0) {
                 System.out.print((arr[x] + 1) + " ");
             } else System.out.print(arr[x] + " ");
+         }
         }
-    }
+
 
 
     private static void task2() {
@@ -27,7 +28,10 @@ public class Main {
             arr[1] = 2;
             arr[2] = 3;
             for (int x = arr.length - 1; x >= 0; x--) {
-                System.out.print(arr[x] + ", ");
+                if (x < arr.length - 1) {
+                    System.out.print(", ");
+                }
+                System.out.print(arr[x]);
             }
             System.out.println();
         }
@@ -35,7 +39,10 @@ public class Main {
         {
             double[] arr = {1.57, 7.654, 9.986};
             for (int x = arr.length - 1; x >= 0; x--) {
-                System.out.print(arr[x] + ", ");
+                if (x < arr.length - 1) {
+                    System.out.print(", ");
+                }
+                System.out.print(arr[x]);
             }
             System.out.println();
 
@@ -44,7 +51,10 @@ public class Main {
         {
             int[] arr = {15, 18, 30, 78, 5};
             for (int x = arr.length - 1; x >= 0; x--) {
-                System.out.print(arr[x] + ", ");
+                if (x < arr.length - 1) {
+                    System.out.print(", ");
+                }
+                System.out.print(arr[x]);
             }
             System.out.println();
         }
@@ -58,22 +68,35 @@ public class Main {
             arr[1] = 2;
             arr[2] = 3;
             for (int x = 0; x < arr.length; x++) {
-                System.out.print(arr[x] + ", ");}
+                if (x > 0) {
+                    System.out.print(", ");
+                }
+                System.out.print(arr[x]);
+            }
             System.out.println();
         }
 
         {
             double[] arr = {1.57, 7.654, 9.986};
             for (int x = 0; x < arr.length; x++) {
-                System.out.print(arr[x] + ", ");}
+                if (x > 0) {
+                    System.out.print(", ");
+                }
+                System.out.print(arr[x]);
+            }
             System.out.println();
 
         }
         {
             int[] arr = {15, 18, 30, 78, 5};
             for (int x = 0; x < arr.length; x++) {
-                System.out.print(arr[x] + ", ");}
+                if (x > 0) {
+                    System.out.print(", ");
+                }
+                System.out.print(arr[x]);
+            }
             System.out.println();
         }
     }
 }
+
